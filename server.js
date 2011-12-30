fe9e2f1f3ccfef94e28125348228ecd3019e78a2
Remy Sharp <remy@leftlogic.com>
2011-12-30 13:26:45 +0000

    read config onload - pretty sure there is a bug in there though...

--- server.js
+++ server.js
@@ -9,23 +9,28 @@ var http = require('http'),
     },
     proxyServer = httpProxy.createServer(options);
 
-fs.watchFile(configFile, function (curr,prev) {
-  // only looking for file changes
-  if (curr.mtime.toString() === prev.mtime.toString()) return;
-
+function loadConfig() {
   util.log('config updated');
   fs.readFile(configFile, 'utf8', function (err, data) {
     if (!err) {
       try {
         options.router = JSON.parse(data || '{}');
       } catch (e) {
+        util.log('failed to parse config');
         options.router = {};
       }
       proxyServer.close();
+      console.dir(options.router);
       proxyServer = httpProxy.createServer(options);
       proxyServer.listen(process.env.PORT || 80);
     }
   });
+}
+fs.watchFile(configFile, function (curr,prev) {
+  // only looking for file changes
+  if (curr.mtime.toString() === prev.mtime.toString()) return;
+  loadConfig();
 });
 
 proxyServer.listen(process.env.PORT || 80);
+loadConfig();
